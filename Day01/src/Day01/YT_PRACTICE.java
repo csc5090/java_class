06ebc8dc9538491088ab3a9ec88eee75b4e04a25
csc5090 <tkdcjf5090@gmail.com>
2025-08-04 19:09:44 +0900

    형변환 연습

--- Day01/src/Day01/YT_PRACTICE.java
+++ Day01/src/Day01/YT_PRACTICE.java
@@ -3,7 +3,61 @@ package Day01;
 public class YT_PRACTICE {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//double 과 float의 차이
+		double a = 3.14222222222;
+		float f = 3.4232222223f;
+		System.out.println(a);
+		System.out.println(f);
+		
+		// 한번에 주석
+		
+		/*
+		 * long c = 12380912480912l; System.out.println(c);
+		 * 
+		 * boolean csc = true; System.out.println(csc);
+		 */
+		
+		// 형변환.
+		
+		int score = 92;
+		System.out.println(score);
+		System.out.println((float) score);
+		System.out.println((double) score);
+		
+		float score_f = 93.3F;
+		double score_d = 98.2;
+		System.out.println((int)score_f );
+		
+		score = 93 + (int) 98.8;
+		System.out.println(score);
+		score_d = (double) 90 + 130;
+		System.out.println(score_d);
+		
+		//숫자를 문자열로.
+		
+		String c = String.valueOf(93);
+		c = Integer.toString(93);
+		System.out.println(c);
+		System.out.println(c);
+		
+		String c2 = String.valueOf(123);
+		c2 = Double.toString(123);
+		System.out.println(c2);
+		
+		
+		//문자열을 숫자로
+		
+		int a = Integer.parseInt("199");
+		System.out.println(a);
+		
+		
+		
+		
+		
+		
+		
+	
 
 	}
 
