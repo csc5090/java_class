12f7e53d08c28e1188da0b043b14bd991f3f12ee
csc5090 <tkdcjf5090@gmail.com>
2025-08-04 20:52:51 +0900

    조건문을 연습함.
    if if else else switch

--- Day01/src/Day01/YT_PRACTICE.java
+++ Day01/src/Day01/YT_PRACTICE.java
@@ -5,17 +5,91 @@ public class YT_PRACTICE {
 		
 		// 조건문 if else
 		
-		int hour = 15;
-		if (hour < 14) {
-			System.out.println("아이스 아메리카노 + 1");
+		/*
+		 * int hour = 10; if (hour < 14) { System.out.println("아이스 아메리카노 + 1"); } else {
+		 * System.out.println("아이스 아메리카노 디카페인 +1");
+		 * 
+		 * } System.out.println("커피 주문 완료 # 1 ");
+		 */
+		
+		// 오후 2시 이거나 모닝 커피를 마신 경우?
+		
+		/*int hour = 14;
+		
+		boolean MorningCoffee = true;
+		if (hour > 14 || MorningCoffee == true ) {
+			System.out.println("아이스 아메리카노 디카페인 + 1 ");
 		} else {
-			System.out.println("아이스 아메리카노 디카페인 +1");
+			System.out.println("아이스 아메리카노 + 1");*/
+			
+			
+			
+			//밥먹은지 얼마나 지남?
+			
+			/*
+			 * int min = 40 ; if ( min > 30 ) { System.out.println( "양치 ㄱ." ); } else {
+			 * System.out.println( "아직 ㄱㅊ" ); }
+			 */
+				
 		
+		// 철수가 있음. 철수 점수 60 점. 60>합격 60<불합격
+		// 결과 출력.
+		
+		
+		/*
+		 * int score = 60; if ( score >= 60) { System.out.println("합격."); } else {
+		 * System.out.println("불합격"); }
+		 */
+		
+	
+		//철수 점수 60점 60 > = a,   50~60 = b.  50 < = c
+		// 성적을 출력.
+		
+		/*int score = 39;
+		if (score >= 60) {
+			System.out.println("A입니다.");
+		} else if(score > 50 || score < 60){
+			System.out.println("b입니다.");
+		} else {
+			System.out.println("c입니다.");*/
+		
+
+		// 이름은 상철 로 입력. 최상철로 받으면 "최상철입니다."
+		// 상철로 받으면 "최상철이 아입니다."
+		// 그 외엔 전부 출력 x 
+		
+		/*
+		 * String name2 = "상철"; if ( name2 == "상철" ) { System.out.println("최상철이 아닙니다.");
+		 * } else if (name2 == "최상철") { System.out.println("최상철입니다."); }
+		 */
+	
+		
+		
+		int day = 3;
+		switch (day) {
+		case 1 :
+			System.out.println("월요일");
+			break;
+		case 2 : 
+			System.out.println("화요일");
+			break;
+		case 3 : 
+			System.out.println("수요일");
+			break;
+		default:
+			System.out.println("알 수 없는 요일");
 		}
-		System.out.println("커피 주문 완료 # 1 ");
 	}
+}
+
+
 		
-	}
+		
+		
+		
+			
+		
+
 		
 	
 
