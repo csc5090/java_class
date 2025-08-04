394cb34baddad4c8a55df395d309a8f91109b002
csc5090 <tkdcjf5090@gmail.com>
2025-08-04 19:34:37 +0900

    다 지우고 다시 if else 공부함

--- Day01/src/Day01/YT_PRACTICE.java
+++ Day01/src/Day01/YT_PRACTICE.java
@@ -1,64 +1,26 @@
 package Day01;
 
 public class YT_PRACTICE {
-
 	public static void main(String[] args) {
 		
-		//double 과 float의 차이
-		double a = 3.14222222222;
-		float f = 3.4232222223f;
-		System.out.println(a);
-		System.out.println(f);
+		// 조건문 if else
 		
-		// 한번에 주석
+		int hour = 15;
+		if (hour < 14) {
+			System.out.println("아이스 아메리카노 + 1");
+		} else {
+			System.out.println("아이스 아메리카노 디카페인 +1");
 		
-		/*
-		 * long c = 12380912480912l; System.out.println(c);
-		 * 
-		 * boolean csc = true; System.out.println(csc);
-		 */
+		}
+		System.out.println("커피 주문 완료 # 1 ");
+	}
 		
-		// 형변환.
-		
-		int score = 92;
-		System.out.println(score);
-		System.out.println((float) score);
-		System.out.println((double) score);
-		
-		float score_f = 93.3F;
-		double score_d = 98.2;
-		System.out.println((int)score_f );
-		
-		score = 93 + (int) 98.8;
-		System.out.println(score);
-		score_d = (double) 90 + 130;
-		System.out.println(score_d);
-		
-		//숫자를 문자열로.
-		
-		String c = String.valueOf(93);
-		c = Integer.toString(93);
-		System.out.println(c);
-		System.out.println(c);
-		
-		String c2 = String.valueOf(123);
-		c2 = Double.toString(123);
-		System.out.println(c2);
-		
-		
-		//문자열을 숫자로
-		
-		int a = Integer.parseInt("199");
-		System.out.println(a);
-		
-		
-		
-		
-		
-		
+	}
 		
 	
 
-	}
 
-}
+
+		
+	
+
