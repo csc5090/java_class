e305e45cf83ff89b49d2654ccf872d545559baa2
csc5090 <tkdcjf5090@gmail.com>
2025-08-05 12:47:29 +0900

    자바 확장 특수 출력문자
    escape sequence

--- Day01/src/Day01/DataType11.java
+++ Day01/src/Day01/DataType11.java
@@ -1,10 +1,20 @@
 package Day01;
 
+/* 자바 확장 특수 출력문자(escape sequence) 종류 :
+ * \n -> 줄바꿈, \t->탭키만큼 수평을 띄움, \' -> 작은 따옴표, \" -> 큰 따옴표
+ * \\ -> 경로구분, \, \b -> 백스페이스, \f -> 연속용지에서 페이지 넘김
+ * \r -> 커서를 현재줄 맨 앞으로 
+ */
+
 public class DataType11 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("\'대전시\'");
+		System.out.println("\"자바\"");
+		System.out.println("홍길동\t이순신\t강감찬");
+		System.out.println("\n");
+		System.out.println("D:\\"); //D:\로 표현
 	}
 
 }
