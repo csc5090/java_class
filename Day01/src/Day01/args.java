787fdab26df902c5cfed334a8a57b66d496618f6
csc5090 <tkdcjf5090@gmail.com>
2025-08-05 11:00:01 +0900

    args  연구

---
+++ Day01/src/Day01/args.java
@@ -0,0 +1,10 @@
+package Day01;
+
+public class args {
+    public static void main(String[] args) {
+        System.out.println("첫 번째 인자: " + args[0]);
+        System.out.println("두 번째 인자: " + args[1]);
+        
+    }
+    
+}
