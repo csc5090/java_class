5268d6246b19dbb744d277d7b72062224aa7248e
csc5090 <tkdcjf5090@gmail.com>
2025-08-06 11:02:20 +0900

    2번 문제를 풀었음

---
+++ Day01/src/제3장문제풀이/DataEx02.java
@@ -0,0 +1,21 @@
+package 제3장문제풀이;
+
+public class DataEx02 {
+
+	public static void main(String[] args) {
+		// 03장. 자료형.pdf no.31 1번 문제.
+		
+		2번 문제.
+		
+		1 -> 127까지 가능.
+		
+		
+		3번 -> 인트에 담길 수 없는 큰 수.
+		형변환 하면 가능.
+		int var3 = (int)28L;
+		
+		
+
+	}
+
+}
