04c93988fff24061be5760bbdea63496d6dd377d
csc5090 <tkdcjf5090@gmail.com>
2025-08-06 10:38:47 +0900

    문제를 풀었음

---
+++ Day01/src/제3장문제풀이/DataEx01.java
@@ -0,0 +1,17 @@
+package 제3장문제풀이;
+
+public class DataEx01 {
+
+	public static void main(String[] args) {
+		
+		// 03장. 자료형.pdf no.31 1번 문제.
+		
+		b -> "" 사용 불가능.
+		d  = > ''는 사용 불가능.
+		ㄷ=> 2 2byte의 한 글자만 지정 가능.
+		
+		
+		
+	}
+
+}
