d25347642a46192101095cf3e0b5ec1dbad5df95
csc5090 <tkdcjf5090@gmail.com>
2025-08-08 10:31:07 +0900

    삼 항 연 산 자

---
+++ Day03/src/Day3/OprTest03.java
@@ -0,0 +1,17 @@
+package Day3;
+
+public class OprTest03 {
+
+	public static void main(String[] args) {
+		
+		boolean flag = true;
+		
+		System.out.printf("flag = %b \n", flag); //true
+		System.out.printf("!true = %b \n", !flag); //!true -> false
+		System.out.printf("!!ture = %b \n", !!flag); //!true ->!false -> true
+		
+		
+
+	}
+
+}
