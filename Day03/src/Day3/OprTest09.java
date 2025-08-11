e152b43e6088aab32571c740f12de52e772d1815
csc5090 <tkdcjf5090@gmail.com>
2025-08-11 09:27:09 +0900

    if, if else, else

---
+++ Day03/src/Day3/OprTest09.java
@@ -0,0 +1,37 @@
+package Day3;
+
+import java.util.Scanner;
+
+public class OprTest09 {
+
+	public static void main(String[] args) {
+		
+		// if else if 다중 조건문으로 학점 구하기 실습.
+		
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("0~100 사이 정수 숫자 점수만 입력 >> ");
+		int score = Integer.parseInt(scan.nextLine());
+		
+		char grade = ' ';//학점
+				
+				if(!(0 <= score && score <= 100)) {
+					System.out.printf("%s \n", "0 부터 100사이 정수 숫자만 입력하세요!");
+				} else {
+				}if(score >= 90 && score <= 100) {
+						grade = 'A';
+					} else if(score >= 80 && score <= 89) {
+						grade = 'B';
+					} else if(score >= 70 && score <= 79) {
+						grade = 'C';
+					}else if(score >= 60 && score <=69) {
+						grade = 'D';
+ 					}else {
+						grade = 'F';
+					}
+					System.out.printf("당신이 입력한 %d점은(는) %c학점입니다. \n", score, grade);
+					//%d는 십진수 정수 출력형태 지시자, %c는 단일문자 출력형태.
+
+					}
+				}
+				
---
+++ Day03/src/Day3/Quiz_05.java
@@ -0,0 +1,13 @@
+package Day3;
+
+public class Quiz_05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = {2,3,4,5,6};
+		//System.out.println(""+a[0]+a[1]+a[2]+a[3]+a[4]);
+		for(int score : a) {
+			
+		}
+	}
+}
