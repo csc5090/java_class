0345e3cbce45d363a0db878edbbbe4c681076da1
csc5090 <tkdcjf5090@gmail.com>
2025-08-11 10:47:23 +0900

    if문 문제 풀이

--- Day03/src/Day3/OprTest12.java
+++ Day03/src/Day3/OprTest12.java
@@ -9,8 +9,7 @@ public class OprTest12 {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("영문 나라 이름 입력(Korea)>>");
 		char first_naraName = scan.nextLine().charAt(0); // 메서드를 한줄로 엮어서 연이어서 코드라인을 줄여 사용
-														//  = 메서드 체이닝.
-		
+														//  = 메서드 체이닝.	
 		switch(first_naraName) {
 		case 'k' :
 		case 'K' : System.out.println("대한민국"); break;
@@ -20,7 +19,5 @@ public class OprTest12 {
 		case 'C' : System.out.println("캐나다"); break;
 		default: System.out.println("해당하는 나라 이름이 없습니다.");
 		}
-
 	}
-
 }
---
+++ Day03/src/Day3/OprTest13.java
@@ -0,0 +1,27 @@
+package Day3;
+
+import java.util.Scanner;
+
+public class OprTest13 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * 스캐너를 활용해서 정수숫자를 입력받은 다음 if조건문을 사용하여 숫자 0을 입력했을 때 '입력한 숫자는
+		 * 0입니다."라 출력되게 하고, 0이 아닐 때는 "입력하신 숫자는 0이 아닙니다."라고 출력.
+		 */
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("숫자를 입력하세요 >> ");
+		int input = Integer.parseInt(scan.nextLine());
+
+		if (input == 0) {
+			System.out.print("입력하신 숫자는 0입니다.");
+		}
+		if (input != 0) {
+			System.out.print("입력하신 숫자는 0이 아닙니다.");
+		}
+
+	}
+
+}
