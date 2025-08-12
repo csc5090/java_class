7f1f9d5fb0acecb207ec95b419110e8952148b33
csc5090 <tkdcjf5090@gmail.com>
2025-08-12 19:04:49 +0900

    FOR문 역피라미드 공부

--- Day04/src/Day04_/LoopEx13.java
+++ Day04/src/Day04_/LoopEx13.java
@@ -26,11 +26,11 @@ public class LoopEx13 {
 		 * System.out.print(" #"); } System.out.println(); }
 		 */
 		
-		for(int i = 1; i <= lineNumber; i++) {
+		for(int i = lineNumber; i >= 1; i--) {
 			for(int j = 1; j <= i; j++) {
-				System.out.print(" ");
+				System.out.print("*");
 			}
-		System.out.println("*");
+		System.out.println("");
 	}
 		
 		
