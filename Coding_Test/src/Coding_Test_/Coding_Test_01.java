f7da57c4a96d627e88d347f2679563c79ed4a7bd
csc5090 <tkdcjf5090@gmail.com>
2025-08-21 20:08:44 +0900

    123

--- Coding_Test/src/Coding_Test_/Coding_Test_01.java
+++ Coding_Test/src/Coding_Test_/Coding_Test_01.java
@@ -4,11 +4,18 @@ public class Coding_Test_01 {
 
 	public static void main(String[] args) {
 		
+		int[] ABC = new int[5];
+		ABC[0] = 1;
+		ABC[1] = 2;
+		ABC[2] = 3;
+		ABC[3] = 4;
+		ABC[4] = 5;
 		
-		
-		
-		
+		for (int i=0; i<ABC.length; i++) {
+			ABC[i] = i * 2;
+			System.out.print(ABC[i]);
 
+		}
 	}
 
 }
