9678432ee853a519bc9f0cafabaf2a55e27a3ef6
csc5090 <tkdcjf5090@gmail.com>
2025-08-26 12:41:35 +0900

    about Method

---
+++ Day10/src/src/MethodEx05.java
@@ -0,0 +1,29 @@
+package src;
+
+/* 참조변수만 선언하고 객체주소를 대입 안 한 상태에서 해당 클래스 소속 멤버변수(인스턴스 변수 : 객체 생성해서 접근하는 변수->속성)
+ * 
+ * 
+ * 
+ */
+
+class Member05 {
+	String id;
+	String name;
+	String phone;
+}
+
+
+
+public class MethodEx05 {
+
+	public static void main(String[] args) {
+		
+		Member05 m; // 레퍼런스 변수만 선언(객체주소가 저장 안 됨)
+		System.out.println("회원 아이디 : "+m.id+", 회원 이름: "m.name+", 폰번호 : "+m.phone);
+		
+		m=new Member05();//객체주소가 저장된 참조변수 m->객체
+		System.out.println("회원 아이디 : "+m.id+", 회원 이름: "m.name+", 폰번호 : "+m.phone);
+
+	}
+
+}
---
+++ Day10/src/src/MethodEx06.java
@@ -0,0 +1,29 @@
+package src;
+
+/* 기본 자료형과 레퍼런스 참조형의 차이점 01)
+ * 기본타입(기본자료형)으로 선언된 변수는 값을 저장하고, 참조타입으로 선언된 변수에는 객체주소를 저장해서 주소값을 공유하여 값도 함께 공유 수정 가능. 
+ */
+
+class MyDate05 {
+	int year = 2024;
+	int month = 10;
+	int day = 31;
+
+}// Mydate05 class
+
+public class MethodEx06 {
+
+	public static void main(String[] args) {
+
+		int x = 7;
+		int y = x; // 7저장
+
+		MyDate05 d = new MyDate05();
+		MyDate05 t = d; // t변수에 d객체주소를 대입. t는 d와 같은 객체주소를 가리키게 됨.
+		System.out.println("x=" + x + ", y=" + y); // 7,7
+		System.out.println(d.year + "년 " + d.month + "월 " + d.day + "일"); //이  은  은  과
+		System.out.println(t.year + "년 " + t.month + "월 " + t.day + "일"); //  둘  같  결
+
+	}
+
+}
