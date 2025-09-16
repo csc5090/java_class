206234ad965ae94edc7185cc6c4c231d1690358b
csc5090 <tkdcjf5090@gmail.com>
2025-09-16 17:27:41 +0900

    리스너

--- Day21/src/중첩클래스와중첩인터페이스/Anonymous.java
+++ Day21/src/중첩클래스와중첩인터페이스/Anonymous.java
@@ -3,10 +3,37 @@ package 중첩클래스와중첩인터페이스;
 //Person 클래스를 활용한 익명객체 구현
 
 public class Anonymous {
-	
-	//필드 초기값으로 대입
-	Person field = new Person() {
-		
-	};
 
+	// 필드 초기값으로 대입
+	Person field = new Person() {
+
+		void work() {
+			System.out.println("출근을 합니다.");
+		}
+
+		@Override
+		void wake() {
+			System.out.println("6시에 일어납니다.");
+			work();
+		}
+	}; // 첫번째 익명 클래스(외부클래명$번호.class -> Anonymous$1.class)
+
+	void method() {
+		// 지역 변수값으로 대입
+		Person localVar = new Person() {
+
+			void walk() {
+				System.out.println("산책을 합니다.");
+			}
+
+			@Override
+			void wake() {
+				walk();
+			}
+		};// 두번째 익명 클래스(외부클래스명$번호.class -> Anonymous$2.class
+	}//mothod()
+	
+	void method02(Person person) {
+		person.wake();
+	}
 }
---
+++ Day21/src/중첩클래스와중첩인터페이스/AnonymousExample.java
@@ -0,0 +1,10 @@
+package 중첩클래스와중첩인터페이스;
+
+public class AnonymousExample {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
